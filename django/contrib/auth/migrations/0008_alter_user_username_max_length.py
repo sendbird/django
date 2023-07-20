31764f8a7f6624c4dafa13dbb58ea1b21c298a8e
Iggy Eom <iggy.eom@sendbird.com>
2023-07-20 13:29:22 -0700

    Fix migration conflicts.

---
+++ django/contrib/auth/migrations/0008_alter_user_username_max_length.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0007_alter_user_username_max_length'),
+    ]
+
+    operations = [
+        # Dummy migration file to make minimal conflict with upstream.
+    ]
